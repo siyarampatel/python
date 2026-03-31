9858fb2af301f8d4a2da23dfd6eb1f8d85f3c1f3
siyarampatel <siyarampateljbp@gmail.com>
2026-03-31 14:46:10 +0530

    basics

--- class1/main.py
+++ class1/main.py
@@ -1,2 +1,22 @@
-name = input("Enter your name:")
-print("your name is",name)
+# name = input("Enter your name:")
+# print("your name is",name)
+
+# def greet(name):
+#  print("ram ram",name)
+
+# greet("abhijeet")
+
+# prime number code
+
+
+number = int(input("Enter your number"))
+if number>1:
+    for i in range(2,number//2):
+     if number%i==0:
+      print("It's not an prime number")
+      break
+    else:
+     print("It's an prime number")
+
+else:
+    print("Tt's not an prime nunber")
